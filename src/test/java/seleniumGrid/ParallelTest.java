456d633b9b46648fd6ee51504f55913dcd4878b2
Banti nayak <bantinayak888@gmail.com>
2024-11-10 15:06:12 +0530

    URL constructor update

--- src/test/java/seleniumGrid/ParallelTest.java
+++ src/test/java/seleniumGrid/ParallelTest.java
@@ -3,6 +3,8 @@ package seleniumGrid;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -14,23 +16,23 @@ public class ParallelTest {
 	public WebDriver driver;
 
 	@Parameters({ "bname" })
-
 	@Test
-
-	public void crossBrowserTesting(String bname) throws MalformedURLException, InterruptedException {
+	public void crossBrowserTesting(String bname)
+			throws MalformedURLException, InterruptedException, URISyntaxException {
 		System.out.println("RemoteDriver connectivity is started");
+		URL url = new URI("http://localhost:4444").toURL();
 		if (bname.equals("Chrome")) {
 
 			ChromeOptions option = new ChromeOptions();
-			driver = new RemoteWebDriver(new URL("http://localhost:4444"), option);
+			driver = new RemoteWebDriver(url, option);
 			System.out.println("session created on Chrome");
 		} else if (bname.equals("Firefox")) {
 			FirefoxOptions option = new FirefoxOptions();
-			driver = new RemoteWebDriver(new URL("http://localhost:4444"), option);
+			driver = new RemoteWebDriver(url, option);
 			System.out.println("session created on Firefox");
 		} else if (bname.equals("Edge")) {
 			EdgeOptions option = new EdgeOptions();
-			driver = new RemoteWebDriver(new URL("http://localhost:4444"), option);
+			driver = new RemoteWebDriver(url, option);
 			System.out.println("session created on Edge");
 		}
 		System.out.println("RemoteDriver connectivity is completed");
